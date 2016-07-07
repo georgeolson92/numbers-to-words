41696f6052d5f822d3c3fcde9df5e743d30c9199
George Olson and James Howard <george+james@>
2016-07-07 13:04:54 -0700

    Add functionality to return tens, hundreds and ones places as words

--- Objects/NumberToWords.cs
+++ Objects/NumberToWords.cs
@@ -9,53 +9,53 @@ namespace NumberToWords.Objects
     public int number;
     public string word;
 
-    public Dictionary<int, string> NumberTranslatorOnes = new Dictionary<int, string>()
+    public Dictionary<string, string> NumberTranslatorOnes = new Dictionary<string, string>()
     {
-      {1, "one"},
-      {2, "two"},
-      {3, "three"},
-      {4, "four"},
-      {5, "five"},
-      {6, "six"},
-      {7, "seven"},
-      {8, "eight"},
-      {9, "nine"},
-      {0, "zero"},
-      {11, "eleven"},
-      {12, "twelve"},
-      {13, "thirteen"},
-      {14, "fourteen"},
-      {15, "fifteen"},
-      {16, "sixteen"},
-      {17, "seventeen"},
-      {18, "eighteen"},
-      {19, "nineteen"}
+      {"1", "one"},
+      {"2", "two"},
+      {"3", "three"},
+      {"4", "four"},
+      {"5", "five"},
+      {"6", "six"},
+      {"7", "seven"},
+      {"8", "eight"},
+      {"9", "nine"},
+      {"0", "zero"},
+      {"11", "eleven"},
+      {"12", "twelve"},
+      {"13", "thirteen"},
+      {"14", "fourteen"},
+      {"15", "fifteen"},
+      {"16", "sixteen"},
+      {"17", "seventeen"},
+      {"18", "eighteen"},
+      {"19", "nineteen"}
     };
 
-    public Dictionary<int, string> NumberTranslatorTens = new Dictionary<int, string>()
+    public Dictionary<string, string> NumberTranslatorTens = new Dictionary<string, string>()
     {
-      {10, "ten"},
-      {20, "twenty"},
-      {30, "thirty"},
-      {40, "fourty"},
-      {50, "fifty"},
-      {60, "sixty"},
-      {70, "seventy"},
-      {80, "eighty"},
-      {90, "ninety"},
+      {"10", "ten"},
+      {"20", "twenty"},
+      {"30", "thirty"},
+      {"40", "fourty"},
+      {"50", "fifty"},
+      {"60", "sixty"},
+      {"70", "seventy"},
+      {"80", "eighty"},
+      {"90", "ninety"}
     };
 
-    public Dictionary<int, string> NumberTranslatorHundreds = new Dictionary<int, string>()
+    public Dictionary<string, string> NumberTranslatorHundreds = new Dictionary<string, string>()
     {
-      {100, "one hundred"},
-      {200, "two hundred"},
-      {300, "three hundred"},
-      {400, "four hundred"},
-      {500, "five hundred"},
-      {600, "six hundred"},
-      {700, "seven hundred"},
-      {800, "eight hundred"},
-      {900, "nine hundred"}
+      {"100", "one hundred"},
+      {"200", "two hundred"},
+      {"300", "three hundred"},
+      {"400", "four hundred"},
+      {"500", "five hundred"},
+      {"600", "six hundred"},
+      {"700", "seven hundred"},
+      {"800", "eight hundred"},
+      {"900", "nine hundred"}
     };
 
     public string NumberTranslator(string input)
@@ -64,17 +64,26 @@ namespace NumberToWords.Objects
       // double digits = Math.Floor(Math.Log10(input) + 1);
       Console.WriteLine(input);
       Console.WriteLine(digits);
+
+      string value;
+
       if (digits == "3")
       {
-        return "one hundred";
+        value = NumberTranslatorHundreds[input];
+        Console.WriteLine(value);
+        return NumberTranslatorHundreds[input];
       }
       else if (digits == "2")
       {
-        return "ten";
+        value = NumberTranslatorTens[input];
+        Console.WriteLine(value);
+        return NumberTranslatorTens[input];
       }
       else if (digits == "1")
       {
-        return "one";
+        value = NumberTranslatorOnes[input];
+        Console.WriteLine(value);
+        return NumberTranslatorOnes[input];
       }
       else
       {
